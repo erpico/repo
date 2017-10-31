cb9ea849960125ab6f602a89d636bab5240af70d
Ruslan A. Polukhin <rp@erpico.ru>
2017-10-31 18:20:46 +0300

    delete lfs version

--- core.dll/1.2.25.20/inc/MyDBC.h
+++
@@ -1,5 +0,0 @@
-#include "./MySQLStatement.h"
-#include "./MySQLResultSet.h"
-#include "./SQLException.h"
-
-#define QUERY_BUFFER_SIZE 256
--- erpicoucihelper.exe/1.0.0.1/inc/ErpicoUCIHelper.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "resource.h"
-
-LONG saveHKLMGuid(const TCHAR *registryPath, const TCHAR *option, const GUID &value);
-GUID stringToGUID(const char* guidString);
