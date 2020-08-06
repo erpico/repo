4060478c7a5b9377491fad5f7cbce7e21f37c24b
Ruslan A. Polukhin <rp@erpico.ru>
2020-08-06 16:22:32 +0300

    06.08.2020 16:22:29,79

---
+++ guilib.dll/2.1.5.17/inc/CallWindowSel.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Resource.h"
+// CCallWindowSel dialog
+
+class CCallWindowSel : public CDialogEx
+{
+	DECLARE_DYNAMIC(CCallWindowSel)
+
+public:
+	CCallWindowSel(CWnd* pParent = nullptr);   // standard constructor
+	virtual ~CCallWindowSel();
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MAKE_CALL_SELECT };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_lstPhones;
+	void AddPhone(const char* strPhone);
+	void ClearPhones();
+	virtual void OnCancel();
+	virtual void OnOK();
+	afx_msg void OnLbnDblclkPhones();
+};
